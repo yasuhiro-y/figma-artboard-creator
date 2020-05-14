cb05b504cdb3b5c4bf9028d334541ffef4ed7d9d
yasuhiro-y <yy@terass.com>
2020-05-14 12:48:51 +0900

    shapeCellsToArtboards

--- code.ts
+++ code.ts
@@ -1,76 +1,109 @@
-figma.showUI(__html__);
+figma.showUI(__html__)
 figma.ui.onmessage = async (msg) => {
-  await figma.loadFontAsync({ family: "Roboto", style: "Regular" })
+  await figma.loadFontAsync({ family: 'Roboto', style: 'Regular' })
   if (msg.type === 'create') {
+    const nodes: SceneNode[] = []
 
-    const nodes: SceneNode[] = [];
-
-    let rowIndex: number = 1
     let name: string, width: number, height: number
-    let value: string, row: number, col: number
-    let offsetX: number = 0, offsetY: number = 0
+    let offsetX: number = 0,
+      offsetY: number = 0
     let maxHeight: number = 0
     let margin: number = 200
 
     const cells: any = msg.content
 
-    for (let i: number = 0; i < cells.length; i++) {
-      console.log("<>")
-      value = cells[i]["gs$cell"]["inputValue"]
-      row = Number(cells[i]["gs$cell"]["row"])
-      col = Number(cells[i]["gs$cell"]["col"])
-      console.log(rowIndex, col)
+    const artboards: Artboard[] = shapeCellsToArtboards(cells)
 
-      if (row === rowIndex && col === 1) {
-        name = String(value)
-      } else if (row === rowIndex && col === 2) {
-        width = Number(value)
-        offsetX = offsetX + margin
-      }
-      else if (row === rowIndex && col === 3) {
-        height = Number(value)
-      } else {
-        // 入力エラー
-      }
+    for (let i: number = 0; i < artboards.length; i++) {
+      console.log('<>')
 
-      console.log(name, width, height)
-      if ((i + 1) % 3 === 0 && i != 0) {
-        const artboard = createArtboard(name, width, height, offsetX, offsetY)
-        const artboardNameIndicator: GroupNode = createArtboardNameIndicator(name)
-        artboard.appendChild(artboardNameIndicator)
-        artboardNameIndicator.x = 30
-        artboardNameIndicator.y = 20
-        nodes.push(artboard);
-        nodes.push(artboardNameIndicator);
-        offsetX = offsetX + width
-        maxHeight = maxHeight <= height && height
-      }
+      name = artboards[i].name
+      width = artboards[i].width
+      height = artboards[i].height
 
-      if ((i + 1) % 15 === 0 && i != 0) {
+      const artboard = createArtboard(name, width, height, offsetX, offsetY)
+      const artboardNameIndicator: GroupNode = createArtboardNameIndicator(name)
+      artboard.appendChild(artboardNameIndicator)
+      artboardNameIndicator.x = 30
+      artboardNameIndicator.y = 20
+      nodes.push(artboard)
+      nodes.push(artboardNameIndicator)
+      offsetX = offsetX + width
+      maxHeight = maxHeight <= height && height
+
+      if ((i + 1) % 5 === 0 && i != 0) {
         offsetY = offsetY + maxHeight + margin
         offsetX = 0
         maxHeight = 0
       }
 
-      if ((i + 1) % 3 === 0) {
-        rowIndex++
-      }
-      console.log("</>")
+      console.log('</>')
     }
 
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+    figma.currentPage.selection = nodes
+    figma.viewport.scrollAndZoomIntoView(nodes)
   }
-  figma.closePlugin();
+  figma.closePlugin()
 }
 
-const createArtboard = (name: string, width: number, height: number, offsetX: number, offsetY: number) => {
-  const frame: FrameNode = figma.createFrame();
+interface Artboard {
+  name?: string
+  width?: number
+  height?: number
+}
+
+const shapeCellsToArtboards = (cells: any) => {
+  console.log(cells)
+  const artboards: Artboard[] = []
+  let value: string, row: number, col: number
+  let rowIndex: number = 1
+  let name = ''
+  let width = 0
+  let height = 0
+
+  for (let i = 0; i < cells.length; i++) {
+    value = String(cells[i]['gs$cell']['inputValue'])
+    row = Number(cells[i]['gs$cell']['row'])
+    col = Number(cells[i]['gs$cell']['col'])
+
+    if (row === rowIndex && col === 1) {
+      name = String(value)
+      console.log(name)
+    } else if (row === rowIndex && col === 2) {
+      width = Number(value)
+      console.log(width)
+    } else if (row === rowIndex && col === 3) {
+      height = Number(value)
+      console.log(height)
+    } else {
+      // 入力エラー
+    }
+
+    if ((i + 1) % 3 === 0 && i != 0) {
+      artboards.push({ name, width, height })
+      name = null
+      width = null
+      height = null
+      rowIndex++
+    }
+  }
+
+  return artboards
+}
+
+const createArtboard = (
+  name: string,
+  width: number,
+  height: number,
+  offsetX: number,
+  offsetY: number
+) => {
+  const frame: FrameNode = figma.createFrame()
   frame.x = offsetX
   frame.y = offsetY
   frame.resize(width, height)
   frame.name = name
-  figma.currentPage.appendChild(frame);
+  figma.currentPage.appendChild(frame)
   return frame
 }
 
@@ -78,27 +111,26 @@ const createArtboardNameIndicator = (name: string) => {
   const text = createTextFrame(name)
   const border = createBorder(text.width)
   const group: GroupNode = figma.group([text, border], figma.currentPage)
-  group.name = "Artboard Name"
+  group.name = 'Artboard Name'
   return group
 }
 
-
 const createTextFrame = (name: string) => {
   const frame: TextNode = figma.createText()
-  frame.name = "Name indicator"
+  frame.name = 'Name indicator'
   frame.characters = name
   frame.fontSize = 24
-  frame.name = "Name"
+  frame.name = 'Name'
   frame.fontName = {
-    family: "Roboto",
-    style: "Regular"
+    family: 'Roboto',
+    style: 'Regular',
   }
   return frame
 }
 
 const createBorder = (width: number) => {
   const border: LineNode = figma.createLine()
-  border.name = "border"
+  border.name = 'border'
   border.resize(width, 0)
   border.y = 36
   return border
